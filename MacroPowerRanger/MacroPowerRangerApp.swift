890ed2056b461b916ea9254b75d8cf1102e7af17
Rizki Miftha <riskimifta@gmail.com>
2021-10-14 16:09:09 +0700

    Initial Commit

---
+++ MacroPowerRanger/MacroPowerRangerApp.swift
@@ -0,0 +1,17 @@
+//
+//  MacroPowerRangerApp.swift
+//  MacroPowerRanger
+//
+//  Created by Muhammad Rizki Miftha Alhamid on 10/14/21.
+//
+
+import SwiftUI
+
+@main
+struct MacroPowerRangerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
